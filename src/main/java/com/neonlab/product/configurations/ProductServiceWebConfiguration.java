bff29570585dc3646c42ae6fb9320969c6ae4e63
ritik-agrawal <ritikagrawal1697@gmail.com>
2024-05-07 06:52:40 +0530

    Cors error fix

---
+++ src/main/java/com/neonlab/product/configurations/ProductServiceWebConfiguration.java
@@ -0,0 +1,15 @@
+package com.neonlab.product.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ProductServiceWebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+    }
+
+}
