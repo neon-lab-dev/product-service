b6ae070d2f6a9164bfad57ab644391812519c252
ritik-agrawal <ritikagrawal1697@gmail.com>
2024-05-15 16:09:01 +0530

    Sending the instock products count in response as well.
    Ticket:
    - https://github.com/neon-lab-dev/product-service/issues/46

--- src/main/java/com/neonlab/product/models/responses/ProductReportModel.java
+++ src/main/java/com/neonlab/product/models/responses/ProductReportModel.java
@@ -6,10 +6,16 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ProductReportModel {
 
     private Long totalProducts;
+    private Long totalInStockProducts;
     private Long totalOutOfStockProducts;
+
+    public ProductReportModel(Long totalProducts, Long totalOutOfStockProducts){
+        this.totalProducts = totalProducts;
+        this.totalOutOfStockProducts = totalOutOfStockProducts;
+        this.totalInStockProducts = this.totalProducts - this.totalOutOfStockProducts;
+    }
 
 }
