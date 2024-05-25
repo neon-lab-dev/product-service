f3b6cf75048a5a4242a243bc53e60f259f3eed13
ritik-agrawal <ritikagrawal1697@gmail.com>
2024-05-25 10:32:34 +0530

    Removed unused imports.
    Ticket:
    https://github.com/neon-lab-dev/product-service/issues/43

--- src/main/java/com/neonlab/product/service/CategoryService.java
+++ src/main/java/com/neonlab/product/service/CategoryService.java
@@ -11,7 +11,6 @@ import com.neonlab.product.entities.Category;
 import com.neonlab.product.enums.CategoryType;
 import com.neonlab.product.repository.CategoryRepository;
 import jakarta.transaction.Transactional;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
